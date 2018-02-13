ba8930c1dbcf59b693af2c3e7a71f9f4d9d45a78
Stef Corputty <scorputty@gmail.com>
2018-02-13 09:31:18 +0100

    Some updates for 2018, put ansible outside the VM again, it works but the build takes quite long.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -eux
 
-# Remove Ansible
-yum remove -y ansible
-yum remove -y epel-release
-yum clean all
-
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
